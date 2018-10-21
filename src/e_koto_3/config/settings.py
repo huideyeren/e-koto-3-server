96c21ab5bb07eaa26eefe75da9e5c723365779eb
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-10-21 12:27:29 +0900

    run by h2o and gunicorn.

--- src/e_koto_3/config/settings.py
+++ src/e_koto_3/config/settings.py
@@ -129,5 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join('/', 'static')
+STATICFILES_DIR = '/static'
+STATIC_ROOT = '/static'
