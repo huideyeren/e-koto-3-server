cc649ae3c5fe0db148d35703cbb5a1db6efdaf16
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-11-01 12:44:59 +0900

    [wip] make user app.

--- src/e_koto_3/diary/admin.py
+++ src/e_koto_3/diary/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
 from .models import Post
 
 admin.site.register(Post)
+
--- src/e_koto_3/diary/models.py
+++ src/e_koto_3/diary/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 class Post(models.Model):
+    """いいことを3つ集めた日記のデータ"""
+
     first_text = models.CharField(max_length=25)
     second_text = models.CharField(max_length=25)
     third_text = models.CharField(max_length=25)
     date = models.DateField(auto_now=False, auto_now_add=True)
 
     def __str__(self):
+        """表示するときに文字列で表示する"""
+
         return self.date.strftime('%Y/%m/%d') + 'の投稿' \
             + '\n1: ' + self.first_text \
             + '\n2: ' + self.second_text \
             + '\n3: ' + self.third_text + '\n'
+            
