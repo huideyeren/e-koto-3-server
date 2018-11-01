a32fee2fba77c214dab369010f483c7d50a60318
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-11-01 12:51:43 +0900

    edited

--- src/e_koto_3/config/settings.py
+++ src/e_koto_3/config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'diary.apps.DiaryConfig',
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
