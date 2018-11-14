cc47830947a945a0a277aef8e3d6179c488a7b99
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-11-14 12:52:42 +0900

    [wip]Twitter Login

--- src/e_koto_3/config/settings.py
+++ src/e_koto_3/config/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gunicorn'
+    'social_django',
+    'gunicorn',
+    'diary',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -66,6 +69,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -116,9 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -133,3 +138,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIR = '/static'
 STATIC_ROOT = '/static'
+
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.twitter.TwitterOAuth',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+SOCIAL_AUTH_TWITTER_KEY = os.environ.get('TWITTER_API_KEY')
+SOCIAL_AUTH_TWITTER_SECRET =　os.environ.get('TWITTER_API_SECRET')
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
